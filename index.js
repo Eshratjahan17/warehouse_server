cf529ba8d97fb81ecaf4885f7220b6196bfba262
Eshratjahan17 <eshratjahan701@gmail.com>
2022-08-31 02:27:48 +0600

    2 apis added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion,ObjectId } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.sh2v2dp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -16,6 +16,21 @@ const client = new MongoClient(uri, {
 
 async function run(){
   try{
+     await client.connect();
+     const productCollection = client.db("warehouse").collection("products");
+     ;
+     app.get("/products", async (req, res) => {
+       const query = {};
+       const cursor = productCollection.find(query);
+       products = await cursor.toArray();
+       res.send(products);
+     });
+     app.get("/products/:id", async (req, res) => {
+       const id = req.params.id;
+       const query = { _id: ObjectId(id) };
+       const product = await productCollection.findOne(query);
+       res.send(product);
+     });
 
   }finally{
 
